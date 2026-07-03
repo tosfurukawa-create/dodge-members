ca0b68bfd07879033b459c9d29b0d5bd26ac12e0
tosfurukawa-create <tos_furukawa@shingakukai.org>
2026-07-04 00:50:03 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* 出場メンバー管理 Service Worker
+   Web公開時にオフラインでも動作するようにするためのファイル。
+   キャッシュを更新したいときは CACHE の番号を上げる。 */
+var CACHE = 'dodge-member-cache-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './出場メンバー管理_v1.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+/* ネットワーク優先・失敗時キャッシュ（オンラインなら常に最新、圏外でも動く） */
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      return res;
+    }).catch(function(){
+      return caches.match(e.request, { ignoreSearch: true });
+    })
+  );
+});
